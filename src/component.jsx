a575d938fec5021bc2c97180a417ca169aaa51b7
VijayBharathi27 <vijays123272002@gmail.com>
2022-12-17 16:05:05 +0530

    "react context"

--- src/component.jsx
+++ src/component.jsx
@@ -1,10 +1,29 @@
 import TextField from "@mui/material/TextField";
 import "./custom.css";
+import { useState, createContext, useContext } from "react";
+export const colorContext = createContext();
 
 export default function Component() {
+  const [color, setColor] = useState("");
+
   return (
     <div className="parent">
-      <TextField label="Outlined" variant="outlined" />
+      <colorContext.Provider value={color}>
+        <div className="flex">
+          <TextField label="Outlined" variant="outlined" />
+          <button onClick={() => setColor("red")}>change color</button>
+          <Para />
+        </div>
+      </colorContext.Provider>
     </div>
+  );
+}
+
+function Para() {
+  const color = useContext(colorContext);
+  return (
+    <>
+      <p style={{ color: `${color}` }}>This is a paragraph</p>
+    </>
   );
 }
